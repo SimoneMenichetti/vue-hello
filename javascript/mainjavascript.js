b345e8ac8f19adf42b87bcee6caad76cd6478749
simone menichetti <simon3menichettui95@gmail.com>
2024-06-24 15:13:37 +0200

    bonus:( add img data whit vue -build: style rules css

--- javascript/mainjavascript.js
+++ javascript/mainjavascript.js
@@ -1,7 +1,5 @@
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-// Bonus:
-// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 
 // miles 1 "// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data."
@@ -10,7 +8,7 @@
 
     // Salviamo in una variabile locale la funzione createApp, presa dalla variabile globale Vue.
 
-    const {createApp}= Vue
+    const {createApp}= Vue;
 
     // 2 step
         // Inizializziamo un'instanza dell'applicazione Vue,invocando la funzione createApp
@@ -19,11 +17,15 @@
         data(){
             return{
                 message: 'Hello Massimo & Simone!',
-                
-            }
-        }
+                 imageSrc: 'https://nordicapis.com/wp-content/uploads/The-Progressive-JavaScript-Framework.png'
+
+            };
+        },
         // step 3
      // Invochiamo il metodo .mount() per connettere l'istanza al tag HTML con id #app e renderizzare l'app
 
     }).mount('#app')
         
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
